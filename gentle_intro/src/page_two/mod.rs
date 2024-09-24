9dcc819aaff523f04bfd4acac5b91206fe3391e1
sahil kashetwar <sahilkashetwar24@gmail.com>
2024-09-24 10:08:37 +0530

    Implementation of traits Debug for Person & Iterator for range

--- gentle_intro/src/page_two/mod.rs
+++ gentle_intro/src/page_two/mod.rs
@@ -2,11 +2,12 @@ use crate::greet;
 
 mod tuples;
 mod structs;
+mod traits;
 
 pub fn main() {
     greet::greet("Let's Learn Structs, Enums and Matching!");
     
     tuples::main();
     structs::main();
-    
+    traits::main();   
 }
---
+++ gentle_intro/src/page_two/traits.rs
@@ -0,0 +1,117 @@
+// Traits.
+// Rust structs cannot inherit from other structs; they are all unique types.
+// There is no sub-typing. they are dumb data.
+
+// `Traits` come into the picture to established relationship between types.
+
+use core::fmt;
+
+// we have added new methods `show` to i32 & f64
+trait Show {
+    fn show(&self) -> String;
+}
+
+impl Show for i32 {
+    fn show(&self) -> String {
+        format!("four-byte signed {}", self)
+    }
+}
+
+impl Show for f64 {
+    fn show(&self) -> String {
+        format!("eight-byte float {}", self)
+    }
+}
+
+fn trait_learn() {
+    let answer = 42;
+    let answer_s = answer.show();
+    println!("Answer {answer}");
+    println!("Show : {answer_s}");
+    
+    let float = 5.45;
+    let float_s = float.show();
+    println!("Float {float}");
+    println!("Show: {float_s}");
+}
+
+
+// learning basic traits of the standard library (they tend to hunt in packs)
+
+// as traits has been defined no more needed!
+// #[derive(Debug)] 
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+impl Person {
+    fn new(name: &str, l_name: &str) -> Self {
+        Person {
+            first_name: name.to_string(),
+            last_name: l_name.to_string(),
+        }
+    }
+    
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
+}
+
+// use of standar lib trait to implement fmt.
+
+impl fmt::Debug for Person {
+    // `write!` is useful macro
+    // `f` is anything that implements `Write`
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.full_name())
+    }
+}
+
+fn person_with_traits() {
+    let person = Person::new("sahil", "Learning Rust");
+    
+    println!("Welcome {:?}", person); // formatter added which ouput.
+    // Welcome sahil Learning Rust
+}
+
+// ITERATOR OVER FLOATING POINT RANGE.
+// we know range before `(0..n)` but they don't work for floating point.
+// Iterator is struct with next method.
+
+struct FRange {
+    val: f64,
+    end: f64, 
+    incr: f64
+}
+
+fn range(x1: f64,x2: f64,skip: f64) -> FRange {
+    FRange { val: x1, end: x2, incr: skip }
+}
+
+impl Iterator for FRange {
+    type Item = f64;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = self.val;
+        if res >= self.end {
+            None
+        } else {
+            self.val += self.incr;
+            Some(res)
+        }
+    }
+}
+
+fn range_learn() {
+    for x in range(0.0,1.0, 0.1) {
+        println!("{:.2} ", x);
+    }
+}
+
+
+pub fn main() {
+    trait_learn();   
+    person_with_traits();
+    range_learn();
+}
