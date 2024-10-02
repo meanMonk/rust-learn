95b7f2951eacf3a44bee8779f03adfbfa23e3922
sahil kashetwar <sahilkashetwar24@gmail.com>
2024-10-02 12:29:42 +0530

    prime number

---
+++ exercism/rust/nth-prime/src/lib.rs
@@ -0,0 +1,6 @@
+pub fn nth(n: u32) -> u32 {
+    todo!("What is the 0-indexed {n}th prime number?")
+    
+    // have function to check for the prime number
+    // have function to iterate and nth number by checking multiple prime number iteratively.
+}
