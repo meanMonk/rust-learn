6976c0d0483dea090dc64b9159b17a3410f64b07
sahil kashetwar <sahilkashetwar24@gmail.com>
2024-10-02 11:10:31 +0530

    fix solve with ternery operator

--- exercism/leap/src/lib.rs
+++ exercism/leap/src/lib.rs
@@ -1,3 +1,7 @@
 pub fn is_leap_year(year: u64) -> bool {
-    (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))
+    if year % 100 == 0 {
+        year % 400 == 0
+    } else {
+        year % 4 == 0
+    }
 }
