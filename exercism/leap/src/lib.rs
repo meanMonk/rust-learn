5b0ee33647bc1874c26f1c03644494bc963cf3f1
sahil kashetwar <sahilkashetwar24@gmail.com>
2024-10-02 11:08:59 +0530

    oneliner solution

--- exercism/leap/src/lib.rs
+++ exercism/leap/src/lib.rs
@@ -1,15 +1,3 @@
 pub fn is_leap_year(year: u64) -> bool {
-    if year % 100 == 0 {
-        if year % 400 == 0 {
-            return true;
-        } else {
-            return false;
-        }
-    } 
-    
-    if year % 4 == 0 {
-        return true;
-    }
-    
-    false
+    (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))
 }
