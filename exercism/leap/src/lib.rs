9d454155b5b5c9e024be2c1ff2f1d85796441302
sahil kashetwar <sahilkashetwar24@gmail.com>
2024-10-02 11:06:08 +0530

    added git ignore and exercism leap year program solve

--- exercism/leap/src/lib.rs
+++ exercism/leap/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn is_leap_year(year: u64) -> bool {
-    todo!("true if {year} is a leap year")
+    if year % 100 == 0 {
+        if year % 400 == 0 {
+            return true;
+        } else {
+            return false;
+        }
+    } 
+    
+    if year % 4 == 0 {
+        return true;
+    }
+    
+    false
 }
