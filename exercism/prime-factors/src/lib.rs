b7983ad72adcd8e13fc67a15386420e04b1214f9
sahil kashetwar <sahilkashetwar24@gmail.com>
2024-10-02 10:56:54 +0530

    prime-factor solved with while loop!

--- exercism/prime-factors/src/lib.rs
+++ exercism/prime-factors/src/lib.rs
@@ -3,6 +3,29 @@ pub fn factors(n: u64) -> Vec<u64> {
     let mut prime_factors = Vec::<u64>::new();
 
     let mut division = n;
+    
+    let mut divisor = 2;
+    
+    while division > 1 {
+        if division % divisor  == 0 {
+            prime_factors.push(divisor);
+            division /= divisor;
+        } else {
+            divisor +=1;
+        }
+    }
+    
+    prime_factors
+}
+
+
+// solution 1
+/* 
+
+    // prepare vec of u64 to push values of prime factors.
+    let mut prime_factors = Vec::<u64>::new();
+
+    let mut division = n;
 
     for divisor in 2..=n {
         while division % divisor == 0 {
@@ -16,4 +39,6 @@ pub fn factors(n: u64) -> Vec<u64> {
     }
     
     prime_factors
-}
+
+    
+*/
