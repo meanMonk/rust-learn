99b634152fef9d5bb32365f178b62380ccabf822
sahil kashetwar <sahilkashetwar24@gmail.com>
2025-04-09 14:45:05 +0530

    learning traits and modules

--- 00_learning/traits/src/aggregator/mod.rs
+++ 00_learning/traits/src/aggregator/mod.rs
@@ -1,6 +1,31 @@
+// pub trait Summary {
+//     fn summarize(&self) -> String;
+// }
+
+// Default Implementations
+// It's useful to have default behaviour for some or all of the methods in trait instead of requiring implementations for all methods on every type.
+
 pub trait Summary {
-    fn summarize(&self) -> String;
+    
+    fn summarize_author(&self) -> Self;
+    
+    fn summarize(&self) -> String {
+        String::from("(Read more..)")
+    }
+    
+    
+    
+};
+
+// to use default implementation of methods we called it like 
+
+pub struct LinkedinPost {
+    pub content: String,
+    pub author: String,
+    pub likes : i32,
 }
+
+impl Summary for LinkedinPost {}
 
 pub struct NewsArticle {
     pub headline: String,
@@ -25,5 +50,54 @@ pub struct Tweet {
 impl Summary for Tweet {
     fn summarize(&self) -> String {
         format!("{}: {} - reply {} retweet {} ", self.username, self.content, self.reply, self.retweet)
+    }
+}
+
+
+// Traits as parameters. 
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news {}", item.summarize());
+}
+
+// Traits bound syntax
+
+pub fn bound_notify<T: Summary>(item: &T) {
+    println!("Breaking news {}", item.summarize());
+}
+
+// Multiple traits bound syntax.
+
+pub fn multi_bound_notify<T: Summary + Display> (item: &T) {
+    
+}
+
+// clearer trait bounds with where clauses.
+
+fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32{
+    
+}
+
+// with use of where clauses.
+
+fn some_function<T, U>(t: &T, u:&U) -> i32 
+where 
+    T: Display + Clone, 
+    U: Clone + Debug,
+
+{
+    
+}
+
+// Returning types that implements traits..
+
+fn returning_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
     }
 }
--- 00_learning/traits/src/main.rs
+++ 00_learning/traits/src/main.rs
@@ -2,7 +2,23 @@
 // we can use traits to defin shared behaviour in an abstract way.
 // Note: Traits are similar to a feature called interfaces in other languages.
 
-use aggregator::{Tweet, NewsArticle, Summary};
+// make sure to bring the trait summary in scope.
+
+/* 
+But we can’t implement external traits on external types. 
+For example, we can’t implement the Display trait on Vec<T> within 
+our aggregator crate because Display and Vec<T> are both defined
+ in the standard library and aren’t local to our aggregator crate. 
+ This restriction is part of a property called coherence, 
+ and more specifically the orphan rule, so named because 
+ the parent type is not present. This rule ensures that other people’s code can’t break your code and vice versa. Without the rule, two crates could implement the same trait for the same type, and Rust wouldn’t know which implementation to use.
+*/
+
+
+
+
+use aggregator::{Tweet, NewsArticle, Summary, LinkedinPost};
+
 
 mod aggregator;
 
@@ -23,7 +39,14 @@ fn main() {
         retweet: false,
     };
     
+    let post = LinkedinPost {
+        content: String::from("rust is awesome as of  now!"),
+        author: String::from("rust_author"),
+        likes: 24,
+    };
     
+    
+    println!("1 linkedin post: {}", post.summarize());
     println!("1 new tweet: {}", tweet.summarize());
     println!("1 news articles: {}", news.summarize());
 }
